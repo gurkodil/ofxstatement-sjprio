11c5ee269a10350d451a35c3625321afb4dd3c5b
gustaf <18744409+gurkodil@users.noreply.github.com>
2024-07-15 14:09:00 +0200

    Format

--- tests/test_sjprio.py
+++ tests/test_sjprio.py
@@ -16,13 +16,13 @@ def test_sjprio() -> None:
     statement = parser.parse()
 
     assert statement is not None
-    assert statement.account_id == '123456******1337'
-    assert statement.currency == 'SEK'
-    assert statement.bank_id == 'SJPRIO'
+    assert statement.account_id == "123456******1337"
+    assert statement.currency == "SEK"
+    assert statement.bank_id == "SJPRIO"
 
     assert len(statement.lines) == 1
     stmt_line = statement.lines[0]
 
-    assert stmt_line.memo == 'ICA Nära We-Hås - Korpilombolo'
+    assert stmt_line.memo == "ICA Nära We-Hås - Korpilombolo"
     assert stmt_line.amount == 125.42
     assert stmt_line.date == datetime(2020, 7, 6, 0, 0)
